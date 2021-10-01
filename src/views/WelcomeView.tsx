c77ffdb99155dd10edd878cc6553903ffdfb2bb7
Mislav Petrina <petrinamislav@gmail.com>
2021-10-01 18:11:22 +0200

    wrote css for welcome page

--- src/views/WelcomeView.tsx
+++ src/views/WelcomeView.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { useHistory } from 'react-router';
 
+import style from './styles/welcome.module.css';
+
+import logo from 'images/check.svg';
+
 export const WelcomeView: React.FC = () => {
 
   const history = useHistory();
@@ -11,11 +15,25 @@ export const WelcomeView: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1>Todoish</h1>
-      <p>Keep your day-to-day tasks in</p>
-      <p>one place</p>
-      <button onClick={onClick}>Start planning</button>
+    <div className={style.welcomeDiv}>
+      <div className={style.titleDiv}>
+        <h1>Todoish</h1>
+        <img alt='todoish-logo' src={logo} />
+      </div>
+      <div className={style.paragraphDiv}>
+        <p>Keep your day-to-day tasks in</p>
+        <p>one place</p>
+      </div>
+      <button 
+        onClick={onClick}
+        className={style.btn}  
+      >
+        Start planning
+      </button>
+      <footer className={style.footer}>
+        <p>Created by</p>
+        <p>Mislav Petrina © 2021.</p>
+      </footer>
     </div>
   );
 };
